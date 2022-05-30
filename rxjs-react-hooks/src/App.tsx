7c74b66fbdad963513de43054452acc6edf32a8a
Kristian Virmo <kristian.virmo@devisioona.fi>
2022-05-30 17:58:09 +0300

    Added RxJS and tested it out

--- rxjs-react-hooks/src/App.tsx
+++ rxjs-react-hooks/src/App.tsx
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const numbers$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+const squared$ = numbers$.pipe(
+  map(x => x * x)
+)
+
+const cubed$ = numbers$.pipe(
+  map(x => x * x * x)
+)
+
+squared$.subscribe(x => console.log(`Squared ${x}`));
+cubed$.subscribe(x => console.log(`Cubed ${x}`));
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
